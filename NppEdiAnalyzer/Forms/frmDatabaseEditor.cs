079476ce41f1afce5e00b1ae41b93c524c793e8e
cwarg <cwarg@kbpbrands.com>
2023-06-11 14:38:47 -0500

    Database edit functionality has been implemented

--- NppEdiAnalyzer/Forms/frmDatabaseEditor.cs
+++ NppEdiAnalyzer/Forms/frmDatabaseEditor.cs
@@ -12,12 +12,19 @@ namespace Kbg.Demo.Namespace
     partial class frmDatabaseEditor : Form
     {
         private readonly IScintillaGateway editor;
+        private SQLiteCommandBuilder cmdBlder;
+        private SQLiteConnection conn;
+        private SQLiteDataAdapter adap;
+        private System.Data.DataSet ds;
+        private string database;
 
         public frmDatabaseEditor(IScintillaGateway editor)
         {
             this.editor = editor;
             InitializeComponent();
         }
+        
+        
 
         private void cbDatabaseSelector_SelectedIndexChanged(object sender, EventArgs e)
         {
@@ -28,29 +35,46 @@ namespace Kbg.Demo.Namespace
             if (!string.IsNullOrEmpty(curAssemblyFolder))
             {
                 try
+
                 {
-                    using (SQLiteConnection db = new SQLiteConnection(@"Data Source=" + curAssemblyFolder + ";Pooling=true;FailIfMissing=false;Version=3"))
-                    {
-                        db.Open();
-                        var selectedDatabaseQuery = new SQLiteCommand($"select distinct * from {selectedDatabase}", db);
-                        SQLiteDataReader finalQuery = selectedDatabaseQuery.ExecuteReader();
-                        DataTable dataTable1 = new DataTable();
-                        foreach (DataColumn column in dataTable1.Columns) 
-                        {
-                            column.MaxLength = 255;
-                            column.AllowDBNull = true;
-                        }
-                        dataTable1.Load(finalQuery);
-                        dataGridView1.DataSource = dataTable1;
-                        
-                        //TODO: CHANGE MAXLENGTH FROM NULL to -1 IN ALL TABLES
-                    }
+                    // using (SQLiteConnection db = new SQLiteConnection(@"Data Source=" + curAssemblyFolder + ";Pooling=true;FailIfMissing=false;Version=3"))
+                    // {
+                    //     db.Open();
+                    //     var selectedDatabaseQuery = new SQLiteCommand($"select * from {selectedDatabase}", db);
+                    //     SQLiteDataReader finalQuery = selectedDatabaseQuery.ExecuteReader();
+                    //     DataTable dataTable1 = new DataTable();
+                    //     foreach (DataColumn column in dataTable1.Columns) 
+                    //     {
+                    //         column.MaxLength = 255;
+                    //         column.AllowDBNull = true;
+                    //     }
+                    //     dataTable1.Load(finalQuery);
+                    //     dataGridView1.DataSource = dataTable1;
+                    // }
+
+                    conn = new SQLiteConnection();
+                    conn.ConnectionString = @"Data Source=" + curAssemblyFolder +
+                                            ";Pooling=true;FailIfMissing=false;Version=3";
+                    conn.Open();
+                    string sqlQuery = $"select * from {selectedDatabase}";
+                    adap = new SQLiteDataAdapter(sqlQuery, conn);
+                    
+                    adap.TableMappings.Add("Table", "LNK_Elements_Components");
+                    adap.TableMappings.Add("Table1", "LNK_Segments_Elements");
+                    adap.TableMappings.Add("Table2", "MST_Components");
+                    adap.TableMappings.Add("Table3", "MST_Elements");
+                    adap.TableMappings.Add("Table4", "MST_RefTables");
+                    adap.TableMappings.Add("Table5", "MST_Segments");
+
+                    ds = new System.Data.DataSet();
+                    adap.Fill(ds, selectedDatabase);
+                    dataGridView1.DataSource = ds.Tables[selectedDatabase];
+                    database = selectedDatabase;
                 }
                 catch (Exception ex)
                 {
-                    MessageBox.Show(ex.ToString());
+                    MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
-
             }
 
         }
@@ -64,24 +88,35 @@ namespace Kbg.Demo.Namespace
             }
         }
 
-        private void button5_MouseClick(object sender, MouseEventArgs e)
-        {
-            /*
-            string assemblyFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-            string xmlFileName = Path.Combine(assemblyFolder,"AggregatorItems.xml");
-
-            string curAssemblyFolder = new System.Uri(System.Reflection.Assembly.GetExecutingAssembly().CodeBase).LocalPath;
-             */
-
-            string curAssemblyFolder = new System.Uri(System.Reflection.Assembly.GetExecutingAssembly().CodeBase).LocalPath;
-            MessageBox.Show(string.Format("DLL path is: {0}", curAssemblyFolder));
-
-
-        }
-
         //private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         //{
 
         //}
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                cmdBlder = new SQLiteCommandBuilder(adap);
+                adap.Update(ds, database);
+                ds.AcceptChanges();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                cmdBlder = new SQLiteCommandBuilder(adap);
+                ds.AcceptChanges();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
     }
 }
--- NppEdiAnalyzer/Forms/frmDatabaseEditor.designer.cs
+++ NppEdiAnalyzer/Forms/frmDatabaseEditor.designer.cs
@@ -33,6 +33,7 @@ namespace Kbg.Demo.Namespace
             this.cbDatabaseSelector = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnUpdate = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -40,20 +41,19 @@ namespace Kbg.Demo.Namespace
             // 
             this.cbDatabaseSelector.FormattingEnabled = true;
             this.cbDatabaseSelector.Items.AddRange(new object[] { "LNK_Elements_Components", "LNK_Segments_Elements", "MST_Components", "MST_Elements", "MST_RefTables", "MST_Segments" });
-            this.cbDatabaseSelector.Location = new System.Drawing.Point(213, 8);
-            this.cbDatabaseSelector.Margin = new System.Windows.Forms.Padding(2);
+            this.cbDatabaseSelector.Location = new System.Drawing.Point(284, 10);
+            this.cbDatabaseSelector.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.cbDatabaseSelector.Name = "cbDatabaseSelector";
-            this.cbDatabaseSelector.Size = new System.Drawing.Size(176, 21);
+            this.cbDatabaseSelector.Size = new System.Drawing.Size(233, 24);
             this.cbDatabaseSelector.TabIndex = 0;
             this.cbDatabaseSelector.SelectedIndexChanged += new System.EventHandler(this.cbDatabaseSelector_SelectedIndexChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(33, 10);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(44, 12);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(157, 13);
+            this.label1.Size = new System.Drawing.Size(194, 16);
             this.label1.TabIndex = 1;
             this.label1.Text = "Select Which Database To Edit";
             // 
@@ -61,19 +61,33 @@ namespace Kbg.Demo.Namespace
             // 
             this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 34);
+            this.dataGridView1.Location = new System.Drawing.Point(16, 42);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(679, 740);
+            this.dataGridView1.Size = new System.Drawing.Size(905, 863);
             this.dataGridView1.TabIndex = 2;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(785, 912);
+            this.btnUpdate.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(136, 41);
+            this.btnUpdate.TabIndex = 3;
+            this.btnUpdate.Text = "Commit Changes";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
             // 
             // frmDatabaseEditor
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(703, 786);
+            this.ClientSize = new System.Drawing.Size(937, 967);
+            this.Controls.Add(this.btnUpdate);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.cbDatabaseSelector);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frmDatabaseEditor";
             this.Text = "Database Editor";
             this.VisibleChanged += new System.EventHandler(this.FrmDatabaseEditorVisibleChanged);
@@ -81,6 +95,8 @@ namespace Kbg.Demo.Namespace
             this.ResumeLayout(false);
             this.PerformLayout();
         }
+
+        private System.Windows.Forms.Button btnUpdate;
 
         #endregion
 
